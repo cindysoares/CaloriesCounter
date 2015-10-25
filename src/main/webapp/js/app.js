311ae51e2eebf6d779801284a54729f26cc21004
Cindy Soares <cindy.as@gmail.com>
2015-10-25 21:47:20 -0200

    listing meals

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,7 +1,26 @@
 (function(){
-  var user = { name: 'Cindy', meals: [] };
-  var app = angular.module('calories', []);
-  app.controller('CaloriesController', function(){
-	  this.loggedUser = user;
-  });
+	var app = angular.module('calories', []);
+	
+	var user = { 
+			name: 'Cindy', 
+			meals: [{date: new Date(2015, 9, 25, 14), text:'Lunch', calories: 600},
+			        {date: new Date(2015, 9, 25, 14), text: 'breakfast', calories: 200},
+			        {date: new Date(2015, 9, 25, 14), text: 'dinner', calories: 400}
+			        ] 
+	};
+  
+	app.controller('CaloriesController', function(){
+		this.loggedUser = user;
+	});
+  
+	app.controller('SectionController', function() {
+		this.selectedTab = 1;
+		this.setTab = function(newTab) {
+			this.selectedTab = newTab;
+		};
+		this.isSelected = function(tab) {
+			return this.selectedTab === tab;
+		};
+	});
+	
 })();
