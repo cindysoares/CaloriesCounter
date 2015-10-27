ac0770c000f78f51ce71f52b51f194ce3d7da04d
Cindy Soares <cindy.as@gmail.com>
2015-10-27 10:01:58 -0200

    login and logout buttons.

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -3,14 +3,20 @@
 	
 	var user = { 
 			name: 'Cindy', 
-			meals: [{date: new Date(2015, 9, 25, 14), text:'lunch', calories: 600},
-			        {date: new Date(2015, 9, 25, 14), text:'breakfast', calories: 200},
-			        {date: new Date(2015, 9, 25, 14), text:'dinner', calories: 400}
+			meals: [{date: new Date(2015, 9, 25, 14), text:'lunch', calories: 600, id: 1},
+			        {date: new Date(2015, 9, 25, 14), text:'breakfast', calories: 200, id:2},
+			        {date: new Date(2015, 9, 25, 14), text:'dinner', calories: 400, id: 3}
 			        ] 
 	};
   
 	app.controller('CaloriesController', function(){
 		this.loggedUser = user;
+		this.logout = function() {
+			this.loggedUser = {};
+		};
+		this.login = function() {
+			this.loggedUser = user;
+		};
 	});
   
 	app.controller('SectionController', function() {
@@ -24,13 +30,36 @@
 	});
 	
 	app.controller('EditMealController', function() {
+		this.editedMeal = {};
 		this.editMode = false;
 		this.setEditMode = function(value) {
 			this.editMode = value;
 		};
 		this.removeMeal = function(mealToRemove) {
-			this.user.meals.pop(mealToRemove);
+			var index = -1;		
+			var mealsArray = eval( this.loggedUser.meals );
+			for( var i = 0; i < mealsArray.length; i++ ) {
+				if( mealsArray[i].id === mealToRemove.id ) {
+					index = i;
+					break;
+				}
+			}
+			if( index === -1 ) {
+				alert( "Something gone wrong" );
+			}
+			this.users.meals.splice( index, 1 );
+			this.editedMeal = {};
+			this.editMode = false;
 		};
+		this.addMeal = function() {
+			this.loggedUser.meals.push(editedMeal);
+			this.editedMeal = {};
+			this.editMode = false;
+		};
+		this.updateMeal = function() {
+			this.loggedUser.meals.push(editedMeal);
+		};
+
 	});
 	
 })();
