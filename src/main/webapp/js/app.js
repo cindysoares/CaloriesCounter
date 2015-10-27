8879fcfaaa91dfcc16efe71e2ae7ee9547dec497
Cindy Soares <cindy.as@gmail.com>
2015-10-27 00:16:07 -0200

    meal edit screen

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -3,9 +3,9 @@
 	
 	var user = { 
 			name: 'Cindy', 
-			meals: [{date: new Date(2015, 9, 25, 14), text:'Lunch', calories: 600},
-			        {date: new Date(2015, 9, 25, 14), text: 'breakfast', calories: 200},
-			        {date: new Date(2015, 9, 25, 14), text: 'dinner', calories: 400}
+			meals: [{date: new Date(2015, 9, 25, 14), text:'lunch', calories: 600},
+			        {date: new Date(2015, 9, 25, 14), text:'breakfast', calories: 200},
+			        {date: new Date(2015, 9, 25, 14), text:'dinner', calories: 400}
 			        ] 
 	};
   
@@ -20,6 +20,16 @@
 		};
 		this.isSelected = function(tab) {
 			return this.selectedTab === tab;
+		};
+	});
+	
+	app.controller('EditMealController', function() {
+		this.editMode = false;
+		this.setEditMode = function(value) {
+			this.editMode = value;
+		};
+		this.removeMeal = function(mealToRemove) {
+			this.user.meals.pop(mealToRemove);
 		};
 	});
 	
