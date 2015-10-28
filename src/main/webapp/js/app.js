01cb1e40b243d56faf04826628557465a7f34131
Cindy Soares <cindy.as@gmail.com>
2015-10-27 22:19:23 -0200

    using directives

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -45,6 +45,7 @@
 				this.removeMeal = function(mealToRemove) {
 					var index = -1;		
 					var mealsArray = eval( this.loggedUser.meals );
+					alert(mealsArray);
 					for( var i = 0; i < mealsArray.length; i++ ) {
 						if( mealsArray[i].id === mealToRemove.id ) {
 							index = i;
@@ -70,38 +71,5 @@
 			}, controllerAs: 'editMeal'
 		};
 	});
-	
-	app.controller('EditMealController', function() {
-		this.editedMeal = {};
-		this.editMode = false;
-		this.setEditMode = function(value) {
-			this.editMode = value;
-		};
-		this.removeMeal = function(mealToRemove) {
-			var index = -1;		
-			var mealsArray = eval( this.loggedUser.meals );
-			for( var i = 0; i < mealsArray.length; i++ ) {
-				if( mealsArray[i].id === mealToRemove.id ) {
-					index = i;
-					break;
-				}
-			}
-			if( index === -1 ) {
-				alert( "Something gone wrong" );
-			}
-			this.users.meals.splice( index, 1 );
-			this.editedMeal = {};
-			this.editMode = false;
-		};
-		this.addMeal = function() {
-			this.loggedUser.meals.push(editedMeal);
-			this.editedMeal = {};
-			this.editMode = false;
-		};
-		this.updateMeal = function() {
-			this.loggedUser.meals.push(editedMeal);
-		};
-
-	});
-	
+		
 })();
