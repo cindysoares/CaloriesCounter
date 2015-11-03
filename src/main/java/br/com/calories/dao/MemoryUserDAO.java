44aae3445fbbe256c71564a5956a021b5e5500dd
Cindy Soares <cindy.as@gmail.com>
2015-11-03 10:54:49 -0200

    adding some meals

--- src/main/java/br/com/calories/dao/MemoryUserDAO.java
+++ src/main/java/br/com/calories/dao/MemoryUserDAO.java
@@ -1,8 +1,10 @@
 package br.com.calories.dao;
 
+import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.Set;
 
+import br.com.calories.model.Meal;
 import br.com.calories.model.Profile;
 import br.com.calories.model.User;
 
@@ -11,7 +13,23 @@ public class MemoryUserDAO implements UserDAO {
 	private static Set<User> users = new HashSet<User>();
 	
 	static {
-		users.add(new User("Cindy Soares", "cindy@email.com", "senha", 1800, Profile.USER));
+		User regular_user = new User("Cindy Soares", "cindy@email.com", "senha", 1800, Profile.USER);
+		regular_user.addMeal(new Meal(new GregorianCalendar(2015, 10, 2, 8, 30).getTime(),
+				"breakfast", 250));
+		regular_user.addMeal(new Meal(new GregorianCalendar(2015, 10, 2, 12, 30).getTime(),
+				"lunch", 415));
+		regular_user.addMeal(new Meal(new GregorianCalendar(2015, 10, 2, 19, 30).getTime(),
+				"dinner", 550));
+		regular_user.addMeal(new Meal(new GregorianCalendar(2015, 10, 3, 8, 30).getTime(),
+				"breakfast", 300));
+		regular_user.addMeal(new Meal(new GregorianCalendar(2015, 10, 3, 12, 30).getTime(),
+				"lunch", 500));
+		regular_user.addMeal(new Meal(new GregorianCalendar(2015, 10, 3, 19, 30).getTime(),
+				"dinner", 600));
+		regular_user.addMeal(new Meal(new GregorianCalendar(2015, 10, 3, 17, 30).getTime(),
+				"snack", 150));
+		
+		users.add(regular_user);
 		users.add(new User("User manager", "manager@email.com", "1234", null, Profile.USER_MANAGER));
 		users.add(new User("User admin", "admin@email.com", "4321", null, Profile.ADMIN_MANAGER));
 	}
