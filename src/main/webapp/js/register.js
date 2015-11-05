82ec6fa2bc28004ada68cd289e9e22d51b693b8d
Cindy Soares <cindy.as@gmail.com>
2015-11-05 00:18:29 -0200

    creating new user.

--- src/main/webapp/js/register.js
+++ src/main/webapp/js/register.js
@@ -1,10 +1,41 @@
 (function() {
 	var registerApp = angular.module('register', ['ngMessages']);
 	
-	registerApp.controller('RegisterCtrl', function($scope) {
+	registerApp.factory('userAddService', function($http) {
+		var myService = {
+				async: function (newUser) {
+					var promise = $http.post("/users/add", null, {params:{
+						name: newUser.name,
+						email: newUser.email,
+						password: newUser.password,
+						profile: 'user'
+					}})
+					.then(function(response){
+						return response.data;
+					});
+					return promise;
+				}
+		};
+		return myService;
+	});
+	
+	registerApp.controller('RegisterCtrl', function($scope, userAddService) {
+		this.$messages = {};
 		this.newUser = {};
 		this.save = function() {
-			$scope.calories.register(this.newUser);
+			if(this.newUser.password != this.newUser.repeatedPassword) {
+				this.$messages.warning = true;
+				return;
+			}
+			userAddService.async(this.newUser).then(function(data) {
+				if(data != null) {
+					$scope.calories.register(data);
+					$scope.register.$messages.saveSuccess = true;
+					this.newUser = {};
+				} else {
+					$scope.register.$messages.warning = true;
+				}
+			});
 		}
 	});
 	
