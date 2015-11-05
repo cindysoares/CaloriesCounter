d808d84e4e87f1579d7c71ff5e25c80367afd4a7
Cindy Soares <cindy.as@gmail.com>
2015-11-05 02:14:45 -0200

    inserting new user.

--- src/main/webapp/js/users.js
+++ src/main/webapp/js/users.js
@@ -1,5 +1,5 @@
 (function() {
-	var usersApp = angular.module('users', []);
+	var usersApp = angular.module('users', ['ngMessages']);
 	
 	usersApp.factory('usersFindAllService', function($http) {
 		var myService = {
@@ -14,11 +14,52 @@
 		return myService;
 	});
 	
-	usersApp.controller('UsersCtrl', function($scope, usersFindAllService) {
+	usersApp.factory('usersAddService', function($http) {
+		var myService = {
+				async: function (newUser) {
+					var promise = $http.post("/users/add", null, {params:{
+						name: newUser.name,
+						email: newUser.email,
+						password: newUser.password,
+						profile: newUser.profile
+					}})
+					.then(function(response){
+						return response.data;
+					});
+					return promise;
+				}
+		};
+		return myService;
+	});
+	
+	usersApp.controller('UsersCtrl', function($scope, usersFindAllService, usersAddService) {
 		this.list = {};
+		this.editMode = false;
+		this.newUser = {}
+		this.$messages = {};
+		this.setEditMode = function(value) {
+			this.editMode = value;
+		};
 		this.init = function() {
 			usersFindAllService.async().then(function(d) {
 				$scope.users.list = d;
+			});
+			this.$messages = {};
+		};
+		this.addUser = function() {
+			if(this.newUser.password != this.newUser.repeatedPassword) {
+				this.$messages.warning = true;
+				return;
+			}
+			usersAddService.async(this.newUser).then(function(data) {
+				if(data != null) {
+					$scope.users.list.push(data);
+					$scope.users.$messages.saveSuccess = true;
+					$scope.users.newUser = {};
+					$scope.users.setEditMode(false);
+				} else {
+					$scope.users.$messages.warning = true;
+				}
 			});
 		};
 		
