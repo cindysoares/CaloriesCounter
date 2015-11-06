ddd36ac5f329c16666e99d96057fde30b6e91905
Cindy Soares <cindy.as@gmail.com>
2015-11-06 20:03:21 -0200

    update settings of selected user as admin

--- src/main/webapp/js/settings.js
+++ src/main/webapp/js/settings.js
@@ -19,11 +19,12 @@
 		this.caloriesLimit = null;
 		this.$messages = {};
 		this.calories = $scope.$parent.$parent.calories;
+		this.selectedUser = null;
 		this.save = function() {			
-			settingsService.async(this.calories.loggedUser.id, this.caloriesLimit).then(function(d) {
+			settingsService.async(this.selectedUser.id, this.caloriesLimit).then(function(d) {
 				this.result = d;
 				if(result) {
-					$scope.settings.calories.loggedUser.caloriesLimit = $scope.settings.caloriesLimit;
+					$scope.settings.selectedUser.caloriesLimit = $scope.settings.caloriesLimit;
 					$scope.settings.$messages.saveSuccess = true;
 				} else {
 					$scope.settings.$messages.warning = true;
@@ -31,12 +32,17 @@
 			});
 		};
 		this.init = function() {
-			this.caloriesLimit = this.calories.loggedUser.caloriesLimit;
+			this.caloriesLimit = this.selectedUser.caloriesLimit;
 			$scope.settings.$messages = {};
 		};
 		
 		$scope.$on("tabSelected", function(event, args){
 			if (args.selectedTab === 'settings') {
+				if($scope.section.selectedUser) {
+					$scope.settings.selectedUser = $scope.section.selectedUser;
+				} else {
+					$scope.settings.selectedUser = $scope.calories.loggedUser;
+				}
 				$scope.settings.init();	
 			}
 		});
